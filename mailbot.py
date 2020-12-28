6712cb51a925e17078c9c478a4fde67baaae1570
Michael Czajkowski <michael@czajkowski.ws>
2020-12-28 06:53:09 -0500

    In Progress

--- mailbot.py
+++ mailbot.py
@@ -2,6 +2,7 @@ import argparse
 import jaraco.logging
 import logging
 from utils import ConfigFileHelper, ElasticSearchHelper
+from builtins import None
 
 log = logging.getLogger(__name__)
 
@@ -50,31 +51,66 @@ class MailBot:
 
     def executeQueries(self):
         '''
-        This method goes through every configured query and executes it on Elastic Search .....
+        This method goes through every configured query and executes it on Elastic Search. It calls parseResult on each query's full result which then scores the results.        
+
+        @see parseResult
         '''
         # Check to see if queries is set in the configuration, first.
         if 'queries' not in self.conf:
             log.warn('No queries were put in the configuration file. Nothing happens.')
             return
         for query in self.conf['queries']:
-            # Execute this query in Elastic Search and print the resuts out for now
+            # Execute this query in Elastic Search and print the resuts out for now.
+            # First check for key components of the query that have to be there for Mailbot to function.
+            if 'query' not in query:
+                # This is critically essential. If not there, log a warning and continue on.
+                log.warning('Query doesn\'t have any "query" criteria. Skipping over: '+str(query))
+                continue
+            if 'title' not in query:
+                # Also essential> Title is the unique ID of the query and used prominently in the e-mail
+                log.warning('Query doesn\'t have any "title" criteria. Skipping over '+str(query))
+                continue
             # Pull out the "query" part and stick that in its own dictionary. The rest of it is metadata needed later, e.g. 'title'
+            # Elastic search will not want any of that, it just wants a dictionary with one term: "query"
             queryDict = { "query" : query["query"] }            
             log.debug('Query to Elastic Search is: ' + str(queryDict))
             result = self.elasticSearchHelper.query(queryDict)
             log.debug('Result: ' + str(result))
             # TODO -- seems to be only bringing in small quantities. Paging?
-            # Parse the query by sending each item to 
-            # parseResult(result)
-        # TODO -- now parse the results and built up scores for each of the hits.        
+            self.parseResult(result, title, hitLimit)
+        # TODO -- now parse the results and built up scores for each of the hits.
         # TODO -- then send out the mail
 
-    def parseResult(self, result):
+    def parseResult(self, result, query):
         '''
-        Parses a response from Elastic Search which is a dictionary containing multiple messages. Scores each one and puts it in the globalReply.
-        '''
-        pass
+        Parses a dictionary response from Elastic Search containing multiple messages. Scores each one and puts it in the globalReply. This method can be called on partial results of the supplied query in order to manage paged results from Elastic Search.
         
+        -- result dictionary, the actual reply from Elastic Search containing part of all of the query response. Required. Without this, there's nothing to do and if None a warning will be issued and nothing will happen.
+        -- query dictionary, the input query dictionary from the configuration file. This contains metadata that helps parse the result and properly update globalReply. Without it, processing the result would fail and if None a warning is issued and nothing will happen.
+        '''
+        if result is None:
+            log.warning('No result given to parseReply. Nothing will happen.')
+            return 
+        if query is None:
+            log.warning('No input query given to parseReply. Nothing will happen.')
+            return
+        # Set up entry in the globalReply first, if it doesn't already exist.
+        # This requires title to be in the input query as that is the key in the dictionary.
+        if 'title' not in query:
+            # Also essential> Title is the unique ID of the query and used prominently in the e-mail
+            log.warning('Query doesn\'t have any "title" criteria. Skipping over parsing results for '+str(query))
+            continue
+        if query['title'] not in self.globalReply:
+            # First time set up. Copy in the original query contents because later we'll need the metadata when sending out the mail.
+            globalReply[query['title']] : {}
+            globalReply[query['title']]['originalQuery'] : query
+        # Now score the results found in the result list..
+        
+    def sendMail(self):
+        '''
+        Sends the mail...
+        '''
+        return
 
 def get_args():
     '''
